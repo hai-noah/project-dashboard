747f36fbce6318c0926cf8738272cf17631d5703
thufailvv <muhammedzaki3964@gmail.com>
2025-01-31 08:45:25 -0800

    homepage

--- src/app/_components/home.tsx
+++ src/app/_components/home.tsx
@@ -1,26 +1,56 @@
 'use client'
 import React from 'react'
+import Image from "next/image";
 
 const HomePage = () => {
  
     return (
         <>
-            <div className='bg-[#1c1363] h-[100%]'>
-            Holle
-            </div>
-            {/* <div className="navbar-collapse collapse show" id="navbarSupportedContent" style="">
-          <ul className="navbar-nav">
-            <li className="nav-item active"><a className="nav-link" href="https://www.free-css.com/free-css-templates">Home <span className="sr-only">(current)</span></a></li>
-            <li className="nav-item"><a className="nav-link" href="pages/about.php">About</a></li>
-            <li className="nav-item"><a className="nav-link" href="pages/service.php">Services</a></li>
-            <li className="nav-item"><a className="nav-link" href="pages/why.php">Why Us</a></li>
-            <li className="nav-item"><a className="nav-link" href="pages/team.php">Team</a></li>
-            <li className="nav-item"><a className="nav-link" href="https://www.free-css.com/free-css-templates"><i className="fa fa-user" aria-hidden="true"></i> Login</a></li>
-            <form action="#" method="post" className="form-inline">
-              <button className="btn  my-2 my-sm-0 nav_search-btn" type="submit"><i className="fa fa-search" aria-hidden="true"></i></button>
-            </form>
-          </ul>
-        </div> */}
+          {/* <div className='bg-[black] h-[100%]'>    */}
+            <div>
+               <span className="h-12 w-12 rounded-full">
+                        <Image
+                          width={2000}
+                          height={500}
+                          src="/images/home/User-01.png"
+                          style={{
+                            width: "auto",
+                            height: "auto",
+                          }}
+                          alt="User"
+                          className="full"
+                        />
+                      </span>
+
+               <span className="h-12 w-12 rounded-full">
+                        <Image
+                          width={2000}
+                          height={500}
+                          src="/images/home/User-02.png"
+                          style={{
+                            width: "auto",
+                            height: "auto",
+                          }}
+                          alt="User"
+                          className="full"
+                        />
+                      </span>
+                      
+               <span className="h-12 w-12 rounded-full">
+                        <Image
+                          width={2000}
+                          height={500}
+                          src="/images/home/User-03.png"
+                          style={{
+                            width: "auto",
+                            height: "auto",
+                          }}
+                          alt="User"
+                          className="full"
+                        />
+                      </span>
+                 </div>
+            {/* </div> */}
         </>
     )
 }
